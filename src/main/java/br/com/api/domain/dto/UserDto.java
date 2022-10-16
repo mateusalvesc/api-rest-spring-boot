25db8b577647c6fc1abecd02568707657e55d85e
Mateus Alves Conceição <mateus.alvesc02@gmail.com>
2022-10-16 12:18:37 -0300

    Criação da classe UserDto

---
+++ src/main/java/br/com/api/domain/dto/UserDto.java
@@ -0,0 +1,18 @@
+package br.com.api.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+}
